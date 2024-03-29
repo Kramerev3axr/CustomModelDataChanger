17f511e3922b73daad121cc452f7f71e2530c5bd
Kramerev3axr <105084115+Kramerev3axr@users.noreply.github.com>
2024-03-29 19:18:56 -0400

    fixed formatting issues
    GitHub messed up the formatting of the code, fixed so it looks cleaner to read

--- src/main/java/com/github/kramerev3axr/cmdchanger/Cmdchanger.java
+++ src/main/java/com/github/kramerev3axr/cmdchanger/Cmdchanger.java
@@ -14,30 +14,30 @@ import net.minecraft.text.Text;
 public class Cmdchanger implements ModInitializer {
 	
 	public static final String MOD_ID = "cmdchanger";
-    public static final Logger LOGGER = LoggerFactory.getLogger("cmdchanger");
-    
-    private static int CYAN = 5636095;
-    private static int GOLD = 16755200;
-    private static int DARK_RED = 11141120;
+    	public static final Logger LOGGER = LoggerFactory.getLogger("cmdchanger");
+	    
+    	private static int CYAN = 5636095;
+    	private static int GOLD = 16755200;
+    	private static int DARK_RED = 11141120;
 
 	@Override
 	public void onInitialize() {
 		 CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			 dispatcher.register(CommandManager.literal("custommodeldata")
 					.then(CommandManager.literal("set")
-             			.then(CommandManager.argument("cmd", IntegerArgumentType.integer())
-             					.executes(ctx -> {
-             						ItemStack selectedItem = ctx.getSource().getPlayer().getInventory().getMainHandStack();
-             						int cmdNbt = IntegerArgumentType.getInteger(ctx, "cmd");
+             					.then(CommandManager.argument("cmd", IntegerArgumentType.integer())
+             						.executes(ctx -> {
+	             						ItemStack selectedItem = ctx.getSource().getPlayer().getInventory().getMainHandStack();
+             							int cmdNbt = IntegerArgumentType.getInteger(ctx, "cmd");
              						
-             						selectedItem.getOrCreateNbt().putInt("CustomModelData", cmdNbt);
-             						ctx.getSource().sendFeedback(() -> Text.literal("Set CustomModelData of ")
-             															.append(Text.literal(selectedItem.getName().getString()).withColor(CYAN))
-             															.append(Text.literal(" to "))
-             															.append(Text.literal(String.valueOf(cmdNbt)).withColor(GOLD)), false);
-             						return 1;
-             					})
-             				)
+             							selectedItem.getOrCreateNbt().putInt("CustomModelData", cmdNbt);
+             							ctx.getSource().sendFeedback(() -> Text.literal("Set CustomModelData of ")
+             												.append(Text.literal(selectedItem.getName().getString()).withColor(CYAN))
+             												.append(Text.literal(" to "))
+             												.append(Text.literal(String.valueOf(cmdNbt)).withColor(GOLD)), false);
+             							return 1;
+             						})
+             					)
 					)
 					.then(CommandManager.literal("get")
 							.executes(ctx -> {
@@ -46,9 +46,9 @@ public class Cmdchanger implements ModInitializer {
 								if (selectedItem.hasNbt()) {
 									int cmdNbt = selectedItem.getNbt().getInt("CustomModelData");
 									ctx.getSource().sendFeedback(() ->  Text.literal("")
-																		.append(Text.literal(selectedItem.getName().getString()).withColor(CYAN))
-																		.append((" has a CustomModelData of "))
-																		.append(Text.literal(String.valueOf(cmdNbt)).withColor(GOLD)), false);
+														.append(Text.literal(selectedItem.getName().getString()).withColor(CYAN))
+														.append((" has a CustomModelData of "))
+														.append(Text.literal(String.valueOf(cmdNbt)).withColor(GOLD)), false);
 								}
 								else {
 									ctx.getSource().sendFeedback(() -> Text.literal("Error! Please make sure you have a CustomModelData set!").withColor(DARK_RED), false);
